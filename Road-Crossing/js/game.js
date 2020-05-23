f8d4204ff421ad9cbac05dd1a6eb3c775b646dcf
Airton Toyansk <atoyansk@gmail.com>
2020-05-24 00:49:12 +0300

    Update and Init functions

--- Road-Crossing/js/game.js
+++ Road-Crossing/js/game.js
@@ -12,16 +12,36 @@ let config = {
 // Create a new game, pass the config
 let game = new Phaser.Game(config);
 
-//Load assets
+// Init
+gameScene.init = function() {
+
+    this.gameW = this.sys.game.config.width;
+    this.gameH = this.sys.game.config.height;
+
+    this.playerSpeed = 2;
+}
+
+// Preload
 gameScene.preload = function() {
     this.load.image('background', 'assets/background.png');
     this.load.image('player', 'assets/player.png');
+    this.load.image('enemy', 'assets/dragon.png');
 };
+// Create
 gameScene.create = function() {
-    let gameW = this.sys.game.config.width;
-    let gameH = this.sys.game.config.height;
+    
+    this.bg = this.add.sprite(0, 0, 'background').setPosition(this.gameW/2, this.gameH/2);
 
-    this.add.sprite(0, 0, 'background').setPosition(gameW/2, gameH/2);
+    this.player = this.add.sprite(70, this.gameH/2, 'player').setScale(0.5);
 
-    this.add.sprite(70, 120, 'player').setScale(0.5);
+    this.enemy1 = this.add.sprite(250, 180, 'enemy');
 };
+
+// Update
+gameScene.update = function() {
+
+    if(this.input.activePointer.isDown) {
+        this.player.x += this.playerSpeed;
+    }
+    
+}
