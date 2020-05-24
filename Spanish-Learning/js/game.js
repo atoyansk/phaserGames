fa3e9821d732d154165beaec5a961b2560391fa4
Airton Toyansk <atoyansk@gmail.com>
2020-05-24 23:40:12 +0300

    Spanish learning - beginning

---
+++ Spanish-Learning/js/game.js
@@ -0,0 +1,24 @@
+let gameScene = new Phaser.Scene('Game');
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 360,
+    scene: gameScene,
+    title: 'Spanish Learning Game',
+    pixelArt: false
+};
+
+let game = new Phaser.Game(config);
+
+gameScene.init = function() {
+
+}
+
+gameScene.preload = function() {
+
+}
+
+gameScene.create = function() {
+    
+}
